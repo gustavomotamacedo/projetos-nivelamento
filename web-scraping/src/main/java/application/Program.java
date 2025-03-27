4285012da8f41ef18eea441c2297baf13b6e56a3
gustavomotamacedo <mota.macedo05@gmail.com>
2025-03-27 10:10:26 -0300

    feat: compress archive methods and test

--- web-scraping/src/main/java/application/Program.java
+++ web-scraping/src/main/java/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Program {
 
@@ -28,6 +35,48 @@ public class Program {
         driver = new ChromeDriver(options);
     }
 
+    public static void compressFiles(Path outputFile, Path... files) {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(outputFile))) {
+            for (Path file : files) {
+                zipOutputStream.putNextEntry(new ZipEntry(file.getFileName().toString()));
+                Files.copy(file, zipOutputStream);
+                zipOutputStream.closeEntry();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static Path[] mapFilesToPath() {
+        File downloadsDir = new File(System.getProperty("user.dir") + "\\src\\output");
+        List<File> downloadFiles = Arrays.asList(downloadsDir.listFiles());
+        Path[] pathsArray = downloadFiles.stream().map(file -> file.toPath()).toArray(Path[]::new);
+        return pathsArray;
+    }
+
     public static void main(String[] args) {
+        setupDriver();
+
+        try {
+            driver.get("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos");
+            driver.findElement(By.xpath("/html/body/div[5]/div/div/div/div/div[2]/button[2]")).click(); // close cookies
+
+            WebElement anexoI = driver.findElement(By.xpath("//*[@id=\"cfec435d-6921-461f-b85a-b425bc3cb4a5\"]/div/ol/li[1]/a[1]"));
+            WebElement anexoII = driver.findElement(By.xpath("//*[@id=\"cfec435d-6921-461f-b85a-b425bc3cb4a5\"]/div/ol/li[2]/a"));
+
+            anexoI.click();
+            TimeUnit.SECONDS.sleep(2); // wait seconds to click in the second file
+            anexoII.click();
+            TimeUnit.SECONDS.sleep(30); // wait seconds to complete download
+
+            File outputZipFile = new File(System.getProperty("user.dir") + "\\src\\output.zip");
+            Path[] pathsArray = mapFilesToPath();
+
+            compressFiles(outputZipFile.toPath(), pathsArray);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.quit();
     }
 }
--- web-scraping/src/test/java/ProgramTest.java
+++ web-scraping/src/test/java/ProgramTest.java
@@ -1,16 +1,57 @@
 import application.Program;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 public class ProgramTest {
     @Before
-    public void setUp() {
+    public void webdriverSetup() {
         Program.setupDriver();
     }
 
-    @Test
-    public void test() {
-        Program.driver.get("https://www.google.com");
+    @After
+    public void webdriverQuit() {
         Program.driver.quit();
+    }
+
+    @Test
+    public void openWebdriverTest() {
+        Program.driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void downloadFileTest() throws InterruptedException {
+        Program.driver.get("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos");
+        Program.driver.findElement(By.xpath("/html/body/div[5]/div/div/div/div/div[2]/button[2]")).click(); // close cookies
+
+        WebElement anexoI = Program.driver.findElement(By.xpath("//*[@id=\"cfec435d-6921-461f-b85a-b425bc3cb4a5\"]/div/ol/li[1]/a[1]"));
+        anexoI.click();
+
+        TimeUnit.SECONDS.sleep(20); // wait 20 seconds to complete download
+
+        File outDir = new File(System.getProperty("user.dir") + "\\src\\output");
+        assert outDir.exists();
+        assert outDir.isDirectory();
+        assert outDir.listFiles().length > 0;
+    }
+
+    @Test
+    public void compressFilesTest() {
+        Program.compressFiles(new File(System.getProperty("user.dir") + "\\src\\output.zip").toPath());
+        File outDir = new File(System.getProperty("user.dir") + "\\src\\output.zip");
+        assert outDir.exists();
+    }
+
+    @Test
+    public void compressOutputFilesTest() {
+        Program.compressFiles(new File(System.getProperty("user.dir") + "\\src\\output.zip").toPath(), Program.mapFilesToPath());
+        File outDir = new File(System.getProperty("user.dir") + "\\src\\output.zip");
+        assert outDir.exists();
     }
 }
